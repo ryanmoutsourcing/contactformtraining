5088c08742f1a2f2caa79ede59af5686c4664aa4
ryanfreelanceagency <ryanupworkagency@gmail.com>
2021-09-19 00:09:22 -0400

    erased a bunch of images

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -40,8 +40,8 @@
 						<!-- Logo
 						============================================= -->
 						<div id="logo">
-							<a href="index" class="standard-logo" data-dark-logo="images/logo-dark.png"><img src="images/logo.png" alt="Canvas Logo"></a>
-							<a href="index" class="retina-logo" data-dark-logo="images/logo-dark@2x.png"><img src="images/logo@2x.png" alt="Canvas Logo"></a>
+							<a href="index" class="standard-logo" data-dark-logo="images/bmw.png"><img src="images/bmw.png" alt="Canvas Logo"></a>
+							<a href="index" class="retina-logo" data-dark-logo="images/bmw.png"><img src="images/bmw.png" alt="Canvas Logo"></a>
 						</div><!-- #logo end -->
 
 
