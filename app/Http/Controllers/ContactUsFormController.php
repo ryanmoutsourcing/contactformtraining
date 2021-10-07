15a9fa2d90add8df6adf51aad49a8a2cb41214b3
ryanfreelanceagency <ryanupworkagency@gmail.com>
2021-10-07 09:40:36 -0400

    waiting review of teacher

--- app/Http/Controllers/ContactUsFormController.php
+++ app/Http/Controllers/ContactUsFormController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Contact;
+use Mail;
 
 class ContactUsFormController extends Controller
 {
@@ -26,6 +27,19 @@ class ContactUsFormController extends Controller
             'services' => 'required'
             //e-mail, phone, subject, message, services
         ]);
+
+         //  Send mail to admin
+         \Mail::send('mail', array(
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'phone' => $request->get('phone'),
+            'subject' => $request->get('subject'),
+            'services' => $request->get('services'),
+            'user_query' => $request->get('message'),
+        ), function($message) use ($request){
+            $message->from($request->email);
+            $message->to('digambersingh126@gmail.com', 'Admin')->subject($request->get('subject'));
+        });
 
         // Store data in database
         Contact::create($request->all());
